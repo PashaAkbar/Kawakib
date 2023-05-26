fcc7f27dc394e48567b7dee1dee6713eae215068
Anwaripasha Akbar <pashaakbar52@gmail.com>
2023-05-26 19:20:04 +0700

    revisi apod

--- src/components/apod.jsx
+++ src/components/apod.jsx
@@ -24,10 +24,10 @@ const Apod = () => {
             <h2 className='text-2xl text-gray-100 py-2 cursor-pointer hover:scale-105 duration-200'> Astronomy Picture of the Day</h2>
             <h3 className='text-2xl text-gray-100 pb-2 cursor-pointer hover:scale-105 duration-200'>{data['title']}</h3>
             <div className='flex flex-wrap content-center pb-10'>
-                <div className='w-1/2 my-auto pr-3'>
+                <div className='w-full mx-auto my-auto pr-3'>
                     <img src={data['url']} alt="" className='mx-auto pb-4 cursor-pointer hover:scale-105 duration-200' style={{height:'240px'}} />        
                 </div>
-                <div className='w-1/2 pl-3'>
+                <div className='w-50 pl-3'>
                     <p className='text-md text-gray-100 py-2 cursor-pointer hover:scale-105 duration-200'>{data['explanation']}</p>
                 </div>
                
